7797d4a28fab7f5d16fe7989f5a7b6ad82d68f7b
Nitesh Chaulagain <your-email@example.com>
2026-09-11 01:10:55 +0545

    added images and make new files

---
+++ products/products.js
@@ -0,0 +1,509 @@
+
+export let products = [
+  {
+   "id": "12345678",
+   "image": "",
+   "name": "",
+   "slug": "",
+   "category": "",
+   "subCategory": "",
+   "rating": {
+    "stars": 4.5,
+    "count": 87
+   },
+   "pricing": {
+    "basePrice": 40,
+    "salePrice": 35,
+    "priceUnit": "200gm",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 45,
+    "su": "KG"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "rogar-plus.png",
+   "name": "Rogar Plus 1 Liter",
+   "slug": "rogar-plus",
+   "category": "Agriculture Items",
+   "subCategory": "Insecticides",
+   "rating": {
+    "stars": 5,
+    "count": 87
+   },
+   "pricing": {
+    "basePrice": 1100,
+    "salePrice": 1100,
+    "priceUnit": "/PC",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 45,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "insect-trapper.png",
+   "name": "Insect Trapper 20Pcs - 1 PKT",
+   "slug": "insect-trapper",
+   "category": "Agriculture Items",
+   "subCategory": "Insecticides",
+   "rating": {
+    "stars": 5,
+    "count": 50
+   },
+   "pricing": {
+    "basePrice": 700,
+    "salePrice": 600,
+    "priceUnit": "/pkt",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 50,
+    "su": "PKT"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "insect-trapper.png",
+   "name": "Insect Trapper 20Pcs - 1 PKT",
+   "slug": "insect-trapper",
+   "category": "Agriculture Items",
+   "subCategory": "Insecticides",
+   "rating": {
+    "stars": 5,
+    "count": 50
+   },
+   "pricing": {
+    "basePrice": 700,
+    "salePrice": 600,
+    "priceUnit": "/pkt",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 50,
+    "su": "PKT"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "chap-cutter-machine.jpg",
+   "name": "Chap Cutter Machine - 1 Set",
+   "slug": "chap-cutter-machine",
+   "category": "Tools & Equipments",
+   "subCategory": "Heavy Machines",
+   "rating": {
+    "stars": 5,
+    "count": 2
+   },
+   "pricing": {
+    "basePrice": 50000,
+    "salePrice": 45000,
+    "priceUnit": "/set",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 10,
+    "su": "set"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "oyster-spawn.webp",
+   "name": "Oyster Mushroom Seeds | Europe Strain",
+   "slug": "oyster-mushroom-seeds",
+   "category": "Mushroom Seeds",
+   "subCategory": "Oyster Mushroom Seed",
+   "rating": {
+    "stars": 4.8,
+    "count": 87
+   },
+   "pricing": {
+    "basePrice": 50,
+    "salePrice": 45,
+    "priceUnit": "/200gm",
+   },
+   "inventory": {
+    "sku": "SPN-OM-200GM",
+    "inStock": true,
+    "quantity": 300,
+    "su": "PKT"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "shitake-mushroom-seed.jpg",
+   "name": "Shitake Mushroom Seed - 1KG",
+   "slug": "shitake-mushroom-seed",
+   "category": "Mushroom Seeds",
+   "subCategory": "Shitake Mushroom Seed",
+   "rating": {
+    "stars": 4.9,
+    "count": 20
+   },
+   "pricing": {
+    "basePrice": 350,
+    "salePrice": 350,
+    "priceUnit": "/KG",
+   },
+   "inventory": {
+    "sku": "SPN-SM-1KG",
+    "inStock": true,
+    "quantity": 100,
+    "su": "KG"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12335678",
+   "image": "milky-mushroom-spawn.jpg",
+   "name": "Milky Mushroom Seed - 200GM/PKT",
+   "slug": "milky-mushroom-seed",
+   "category": "Mushroom Seeds",
+   "subCategory": "Milky Mushroom Seed",
+   "rating": {
+    "stars": 4.8,
+    "count": 200
+   },
+   "pricing": {
+    "basePrice": 50,
+    "salePrice": 50,
+    "priceUnit": "/200gm",
+   },
+   "inventory": {
+    "sku": "SPN-MM-200GM",
+    "inStock": true,
+    "quantity": 1000,
+    "su": "pkt"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "button-mushroom-spawn.png",
+   "name": "Button Mushroom Seed/Spawn - 1KG",
+   "slug": "button-mushroom-seed",
+   "category": "Mushroom Seeds",
+   "subCategory": "Button Mushroom Seed",
+   "rating": {
+    "stars": 4.9,
+    "count": 99
+   },
+   "pricing": {
+    "basePrice": 300,
+    "salePrice": 300,
+    "priceUnit": "/KG",
+   },
+   "inventory": {
+    "sku": "SPN-BM-1KG",
+    "inStock": true,
+    "quantity": 50,
+    "su": "KG"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "dextrose-extrapure.png",
+   "name": "Dextrose Extrapure - 500GM",
+   "slug": "dextrose-extrapure",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Culture Media Items",
+   "rating": {
+    "stars": 5,
+    "count": 20
+   },
+   "pricing": {
+    "basePrice": 1250,
+    "salePrice": 990,
+    "priceUnit": "/500gm",
+   },
+   "inventory": {
+    "sku": "DXT-EXTPURE-500GM",
+    "inStock": true,
+    "quantity": 30,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "measuring-jug.png",
+   "name": "Measuring Jug",
+   "slug": "measuring-jug",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Containers",
+   "rating": {
+    "stars": 5,
+    "count": 10
+   },
+   "pricing": {
+    "basePrice": 300,
+    "salePrice": 250,
+    "priceUnit": "200gm",
+   },
+   "inventory": {
+    "sku": "MSG-JUG-1LTR",
+    "inStock": true,
+    "quantity": 45,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "test-tube.png",
+   "name": "Test Tube",
+   "slug": "test-tube",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Test Tubes",
+   "rating": {
+    "stars": 4.9,
+    "count": 99
+   },
+   "pricing": {
+    "basePrice": 45,
+    "salePrice": 35,
+    "priceUnit": "/pc",
+   },
+   "inventory": {
+    "sku": "TST-TB-120MM",
+    "inStock": true,
+    "quantity": 50,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "drill-machine.png",
+   "name": "Drill Machine (Master Pro)",
+   "slug": "drill-machine",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Glassware & Tools",
+   "rating": {
+    "stars": 4.6,
+    "count": 58
+   },
+   "pricing": {
+    "basePrice": 4000,
+    "salePrice": 3500,
+    "priceUnit": "/pc",
+   },
+   "inventory": {
+    "sku": "DRL-MCN-1PC",
+    "inStock": true,
+    "quantity": 10,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "angle-grinder.png",
+   "name": "Angle Grinder(Tonki)",
+   "slug": "angle-grinder",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Glassware & Tools",
+   "rating": {
+    "stars": 4.5,
+    "count": 87
+   },
+   "pricing": {
+    "basePrice": 4000,
+    "salePrice": 3500,
+    "priceUnit": "/pc",
+   },
+   "inventory": {
+    "sku": "ANG-GND-1PC",
+    "inStock": true,
+    "quantity": 20,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "patanjali-herbal-hand-wash.png",
+   "name": "Patanjali Herbal Hand Wash (Anti Bacterial)",
+   "slug": "patanjali-herbal-hand-wash",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Cleaning",
+   "rating": {
+    "stars": 4.7,
+    "count": 65
+   },
+   "pricing": {
+    "basePrice": 170,
+    "salePrice": 150,
+    "priceUnit": "250ml",
+   },
+   "inventory": {
+    "sku": "HND-WSH-250ML",
+    "inStock": true,
+    "quantity": 200,
+    "su": "PCS"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "11345678",
+   "image": "disposable-glove.png",
+   "name": "Disposable Glove",
+   "slug": "disposable-glove",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Gloves",
+   "rating": {
+    "stars": 4.5,
+    "count": 250
+   },
+   "pricing": {
+    "basePrice": 120,
+    "salePrice": 100,
+    "priceUnit": "100pcs",
+   },
+   "inventory": {
+    "sku": "DSP-GLV-100PCS",
+    "inStock": true,
+    "quantity": 75,
+    "su": "BOX"
+   },
+   "keywords": [
+    
+   ]
+  },
+  {
+   "id": "12343677",
+   "image": "",
+   "name": "Face Mask",
+   "slug": "face-mask",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Mask",
+   "rating": {
+    "stars": 5,
+    "count": 20
+   },
+   "pricing": {
+    "basePrice": 150,
+    "salePrice": 100,
+    "priceUnit": "/pkt",
+   },
+   "inventory": {
+    "sku": "FC-MSK-50PCS",
+    "inStock": true,
+    "quantity": 45,
+    "su": "BOX"
+   },
+   "keywords": [
+    
+   ]
+  },  
+  {
+   "id": "12345677",
+   "image": "",
+   "name": "Oyster Mushroom Pickle Jar",
+   "slug": "oyster-mushroom-pickle-jar",
+   "category": "Processed Foods",
+   "subCategory": "Preserves & Pickles",
+   "rating": {
+    "stars": 4.9,
+    "count": 300
+   },
+   "pricing": {
+    "basePrice": 300,
+    "salePrice": 250,
+    "priceUnit": "200gm",
+   },
+   "inventory": {
+    "sku": "FOOD-PKL-500GM",
+    "inStock": true,
+    "quantity": 5,
+    "su": "Jar"
+   },
+   "keywords": [
+    "oyster mushroom pickle",
+    "mushroom pickle",
+    "achar",
+    "achaar",
+    "chyauko achar",
+    "chyau"
+   ]
+  },
+  {
+   "id": "12345678",
+   "image": "",
+   "name": "Rubber Bands (Zero Size) - 1KG PKT",
+   "slug": "rubber-band-zero-size",
+   "category": "Lab & Mycology Supplies",
+   "subCategory": "Rubber",
+   "rating": {
+    "stars": 4.8,
+    "count": 150
+   },
+   "pricing": {
+    "basePrice": 850,
+    "salePrice": 790,
+    "priceUnit": "1KG",
+   },
+   "inventory": {
+    "sku": "SPN-PO-3LB",
+    "inStock": true,
+    "quantity": 500,
+    "su": "KG"
+   },
+   "keywords": [
+    "rubber bands",
+    "zero size rubber",
+    "rubber",
+    "sano rubber",
+    "rabar",
+    "zero rabar",
+    "zero rubber",
+    "rubbers"
+   ]
+  }
+]
