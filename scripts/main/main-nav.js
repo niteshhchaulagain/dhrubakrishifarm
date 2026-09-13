7f2c6da9f2a596bf190b005875ddfcf649b578e0
Nitesh Chaulagain <your-email@example.com>
2026-09-13 16:47:44 +0545

    make sites work on mobile

--- scripts/main/main-nav.js
+++ scripts/main/main-nav.js
@@ -11,21 +11,3 @@ hideBtn.addEventListener('click', () => {
   sidebar.style.display = "none";
 })
 
-document.addEventListener('click', (event) => {
-  const isOpen = sidebar.classList.contains('open');
-
-  const clickedOutside = !sidebar.contains(event.target) && !showBtn.contains(event.target);
-
-  if (isOpen && clickedOutside) {
-    sidebar.classList.remove('open');
-  }
-})
-
-document.addEventListener('click', (event) => {
-  const isOpen = sidebar.style.display === 'flex';
-  const clickedOutside = !sidebar.contains(event.target) && !showBtn.contains(event.target);
-
-  if (isOpen && clickedOutside) {
-    sidebar.style.display = 'none';
-  }
-})
